bb83c74f52fc6120ef2f766cacb102e2cbce17e9
Alberto Rocha <betoharres@gmail.com>
2020-04-14 20:00:17 -0300

    feat: add phone validation to phone model

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -4,6 +4,8 @@ class Phone < ApplicationRecord
   has_many :patients, dependent: :delete_all, inverse_of: :phone
   has_one :volunteer, dependent: :destroy, inverse_of: :phone
 
+  validates :number, presence: true, uniqueness: true
+
   # TODO: send sms also after
   before_create :send_sms_code
 
