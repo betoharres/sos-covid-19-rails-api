d5d7f81e8784d623245dd92958850762db00b895
Alberto Rocha <betoharres@gmail.com>
2020-04-22 11:24:07 -0300

    fix: sending sms code after create phone

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -7,24 +7,23 @@ class Phone < ApplicationRecord
 
   validates :number, presence: true, uniqueness: true
 
+  after_create :send_sms_code
+
   private
 
   def code_valid?(user_sms_code)
     user_sms_code == sms_code
   end
 
-  def generate_new_sms_code!
+  def generate_new_sms_code
     generated_code = rand(0o000..9999).to_s.rjust(4, '0')
     update!(sms_code: generated_code)
     generated_code
   end
 
-  def send_sms_code!
+  def send_sms_code
     self.is_sms_sent = true
-    generate_new_sms_code!
-    TwilioClient.new.send_text(
-      number,
-      "SOS COVID-19 - Seu código: #{generated_code}"
-    )
+    code = generate_new_sms_code
+    TwilioClient.new.send_text(number, "SOS COVID-19 - Seu código: #{code}")
   end
 end
