93f1fa3d2fdc249d2dacb0f3dcd1297029d783cb
Alberto Rocha <betoharres@gmail.com>
2020-04-22 23:43:29 -0300

    refactor: reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  resources :patients
+  resources :volunteers, except: :index
+  # resources :phones
+
   post 'patient_state/call/:patient_id', to: 'patient_state#call'
   post 'patient_state/visit/:patient_id', to: 'patient_state#visit'
   post 'patient_state/discard/:patient_id', to: 'patient_state#discard'
   post 'patient_state/infect/:patient_id', to: 'patient_state#infect'
   post 'patient_state/history/:patient_id', to: 'patient_state#history'
-
-  resources :patients
-  resources :volunteers, except: :index
-  # resources :phones
 
   post '/phones/validate', to: 'phones#validate'
 end
