4a1345eea134dc7933c907ce61bda153bfc6b4ce
Alberto Rocha <betoharres@gmail.com>
2020-04-24 16:16:56 -0300

    hotfix: rename routes and changes route method

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
   post 'patient_state/visit/:patient_id', to: 'patient_state#visit'
   post 'patient_state/discard/:patient_id', to: 'patient_state#discard'
   post 'patient_state/infect/:patient_id', to: 'patient_state#infect'
-  post 'patient_state/history/:patient_id', to: 'patient_state#history'
+
+  get 'patient_state/history/:patient_id', to: 'patient_state#history'
 
   post '/phones/validate', to: 'phones#validate'
-  post 'session/new'
+  post '/login', to: 'session#new'
 end
