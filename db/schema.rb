ad4fa3daba4b516da7969b5b83800153ff9b0281
Alberto Rocha <betoharres@gmail.com>
2020-05-07 07:01:02 -0300

    feat: add description column to patients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_195336) do
+ActiveRecord::Schema.define(version: 2020_05_07_054959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_04_08_195336) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "aasm_state"
+    t.string "description"
     t.index ["phone_id"], name: "index_patients_on_phone_id"
   end
 
