e32389254301b8cca7382b114822227f8ec33073
Alberto Rocha <betoharres@gmail.com>
2020-05-17 03:22:40 -0300

    update: rack-attack config

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,13 +1,17 @@
 module Rack
   class Attack
-    throttle('req/ip', limit: 10, period: 5, &:ip)
+    throttle('req/ip', limit: 6, period: 30, &:ip)
 
     throttle('limit patients creation', limit: 6, period: 3600) do |req|
       req.params['phone'] if req.path == '/patients' && req.post?
     end
 
-    throttle('limit phone validation', limit: 6, period: 60) do |req|
+    throttle('limit phone validation', limit: 10, period: 3600) do |req|
       req.params['number'] if req.path == '/phones/validate' && req.post?
+    end
+
+    throttle('limit phone creation', limit: 6, period: 3600) do |req|
+      req.params['number'] if req.path == '/phones' && req.post?
     end
   end
 end
