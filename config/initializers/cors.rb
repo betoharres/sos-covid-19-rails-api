a9aa9340fe844b9bc1c369478e431e8eaaa59e68
Alberto Rocha <betoharres@gmail.com>
2020-05-24 17:37:21 -0300

    fix: rack-cors localhost port number

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  'ajudacoronavirus.com', 'localhost'
+    origins  'ajudacoronavirus.com', 'localhost:3001'
 
     resource '*', headers: :any,
                   methods: %i[get post put patch delete options head]
