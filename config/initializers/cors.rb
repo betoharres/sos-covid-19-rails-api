a380ab1ca9136824bb321c42025e87cac379be95
Alberto Rocha <betoharres@gmail.com>
2020-05-24 16:48:32 -0300

    feat: change cors to retrict request origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins  'ajudacoronavirus.com', 'localhost'
 
     resource '*', headers: :any,
                   methods: %i[get post put patch delete options head]
